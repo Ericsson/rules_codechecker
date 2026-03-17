143b8c54fbb9b92c073c5d53ba3a0dee1afe7a56
F.Tibor <tibi.furesz@gmail.com>
2026-03-17 12:23:53 +0100

    Skip foss tests not using bzlmod for bazel 8

--- test/foss/yaml-cpp/init.sh
+++ test/foss/yaml-cpp/init.sh
@@ -24,6 +24,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# Skip this test on bazel 8
+MAJOR_VERSION=$(bazel --version | cut -d' ' -f2 | cut -d'.' -f1)
+if [ "$MAJOR_VERSION" -ge 8 ]; then
+    echo "" >> $1/.skipfosstest
+    exit 0
+fi
+
 git clone https://github.com/jbeder/yaml-cpp.git "$1"
 git -C "$1" checkout yaml-cpp-0.7.0
 
@@ -59,11 +66,6 @@ codechecker_test(
 
 #-------------------------------------------------------
 EOF
-# Apply bazel 8 specific flag, forcing the use of WORKSPACE
-VERSION=$(< "../../templates/.bazelversion")
-MAJOR_VERSION=${VERSION%%.*}
-if [ "$MAJOR_VERSION" -eq 8 ]; then
-    echo "common --enable_workspace" >> .bazelrc
-fi
+
 # Add rules_codechecker repo to WORKSPACE
 cat ../templates/WORKSPACE.template >> "$1/WORKSPACE"

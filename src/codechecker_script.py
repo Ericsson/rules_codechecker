ed8a14b41b2268e40613bf22dafd599456834de5
F.Tibor <tibi.furesz@gmail.com>
2026-06-27 10:13:05 +0200

    Resolve clang paths too

--- src/codechecker_script.py
+++ src/codechecker_script.py
@@ -30,8 +30,8 @@ import sys
 EXECUTION_MODE = "{Mode}"
 VERBOSITY = "{Verbosity}"
 CODECHECKER_PATH = os.path.realpath("{codechecker_bin}")
-CLANG_PATH = "{clang_bin}"
-CLANG_TIDY_PATH = "{clang_tidy_bin}"
+CLANG_PATH = os.path.realpath("{clang_bin}")
+CLANG_TIDY_PATH = os.path.realpath("{clang_tidy_bin}")
 CODECHECKER_SKIPFILE = "{codechecker_skipfile}"
 CODECHECKER_CONFIG = "{codechecker_config}"
 CODECHECKER_ANALYZE = "{codechecker_analyze}"

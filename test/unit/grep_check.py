bf4e95d09c463678bc2afba598f22e8c1e2311f8
F.Tibor <tibi.furesz@gmail.com>
2026-07-05 14:55:05 +0200

    Extend unit test with the ability to assert certain files not existing

--- test/unit/grep_check.py
+++ test/unit/grep_check.py
@@ -45,6 +45,12 @@ def parse_args() -> argparse.Namespace:
         help="Path or glob pattern to the file(s) to search within.",
     )
     parser.add_argument(
+        "--no_files",
+        nargs="+",
+        required=False,
+        help="Path or glob pattern to the file(s) that shouldn't exists.",
+    )
+    parser.add_argument(
         "--contains",
         nargs="+",
         required=False,
@@ -75,8 +81,7 @@ def parse_args() -> argparse.Namespace:
 def check_args(args):
     """Checks wether the arguments are correct, aborts if not"""
     if not args.contains and not args.excludes and not args.regex_patterns:
-        print("  [ERROR] Must define at least one pattern or negative pattern.")
-        sys.exit(1)
+        fail("  [ERROR] Must define at least one pattern or negative pattern.")
 
 
 def exact_match(pattern: str, content: str) -> bool:
@@ -143,25 +148,53 @@ def check_file(content: str, args) -> tuple[bool, set[str], set[str]]:
     return all_passed, found_patterns, missing_patterns
 
 
+def assert_files_missing(files):
+    """
+    Asserts that the list of file paths given as parameter, doesn't exists.
+    """
+    for file_pattern in files:
+        matched_files = glob.glob(file_pattern, recursive=True)
+        if matched_files:
+            print(f"\n{file_pattern} exists, however it shouldn't.")
+            sys.exit(1)
+
+
+def fail(msg: str, exit_code: int = 1):
+    """
+    Exits the program, with a message
+    """
+    print(msg)
+    sys.exit(exit_code)
+
 def main() -> None:
     """Entry point for the pattern-matching test."""
     args = parse_args()
     check_args(args)
+
+    assert_files_missing(args.no_files)
+
+    files_to_check = [x for x in args.files if x not in args.no_files]
+    if not files_to_check:
+        print(
+            "There is no files to check, "
+            "since all files are expected to not exits"
+        )
+        sys.exit(0)
 
     all_passed = True
     found_patterns = set()
     missing_patterns = set()
 
     file_paths = []
-    for file_pattern in args.files:
+    for file_pattern in files_to_check:
         matched_files = glob.glob(file_pattern, recursive=True)
         if not matched_files:
-            print(f"  [WARN] No files matched pattern/path: '{file_pattern}'")
+            # Each file pattern must match at least a file
+            fail(f"  [WARN] No files matched pattern/path: '{file_pattern}'")
         file_paths.extend(matched_files)
 
     if not file_paths:
-        print("  [ERR] No file collected to be checked.")
-        sys.exit(1)
+        fail("  [ERR] No file collected to be checked.")
 
     for file in file_paths:
         with open(file, "r", encoding="utf-8") as f:
@@ -186,8 +219,7 @@ def main() -> None:
     if not all_passed:
         for file, pattern in missing_patterns:
             print(f"Missing pattern {pattern} in file {file}")
-        print("\nOne or more patterns missing. Test FAILED.")
-        sys.exit(1)
+        fail("\nOne or more patterns missing. Test FAILED.")
 
 
 if __name__ == "__main__":
--- test/unit/unit_test.bzl
+++ test/unit/unit_test.bzl
@@ -31,6 +31,7 @@ Example:
 def unit_test(
         name,
         files,
+        absent_files = [],
         contains = None,
         excludes = None,
         regex_patterns = None,
@@ -43,6 +44,9 @@ def unit_test(
     Args:
         name: Test name.
         files: Path or glob to the files to be checked.
+        absent_files: Path or glob that should not exists.
+                      If these are in the `files` list too,
+                      disregard the missing file error.
         contains: Text that should be inside the files.
         excludes: Text that shouldn't be inside the files.
         regex_patterns: Regex patterns that should be found inside the files.
@@ -53,6 +57,8 @@ def unit_test(
     """
     if type(files) == "string":
         files = [files]
+    if type(absent_files) == "string":
+        absent_files = [absent_files]
     if type(contains) == "string":
         contains = [contains]
     if type(excludes) == "string":
@@ -61,6 +67,9 @@ def unit_test(
         regex_patterns = [regex_patterns]
 
     python_args = ["--files"] + files
+    if absent_files:
+        python_args.append("--no_files")
+        python_args.extend(absent_files)
     if contains:
         python_args.append("--contains")
         python_args.extend(["\"{}\"".format(pat) for pat in contains])
